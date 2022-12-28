da1d1c2baea7b3099b15c104f5558a994cb9b9ad
mosherif21 <mo.sherif1992@gmail.com>
2022-12-28 20:49:10 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-import 'dart:math';
-
-import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_web_frame/flutter_web_frame.dart';
@@ -13,6 +8,7 @@ import 'package:goambulance/src/constants/app_init_constants.dart';
 import 'package:goambulance/src/features/authentication/screens/login_screen.dart';
 import 'package:goambulance/src/features/home_page/screens/home_page_screen.dart';
 import 'package:goambulance/src/features/onboarding/screens/on_boarding_screen.dart';
+import 'package:goambulance/src/general/notifications.dart';
 import 'package:goambulance/src/routing/splash_screen.dart';
 import 'package:goambulance/src/utils/theme/theme.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
@@ -30,18 +26,7 @@ void main() async {
     await AppInit.initialize();
   }
 
-  if (!AppInit.isWeb) {
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      if (kDebugMode) {
-        print("Handling a foreground message: ${message.notification?.title}");
-      }
-      await _initializeMessaging();
-      await _createNotifications(
-          message.notification!, Random().nextInt(10000));
-    });
-    await FirebaseMessaging.instance.getInitialMessage();
-  }
+  await initializeNotification();
   runApp(const MyApp());
 }
 
@@ -70,58 +55,5 @@ class MyApp extends StatelessWidget {
       enabled: AppInit.notWebMobile,
       backgroundColor: Colors.white,
     );
-  }
-}
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  if (kDebugMode) {
-    print("Handling a background message: ${message.notification?.title}");
-  }
-}
-
-Future<void> _createNotifications(RemoteNotification message, int id) async {
-  await AwesomeNotifications().createNotification(
-      content: NotificationContent(
-    id: id,
-    channelKey: 'goambulance',
-    title: message.title,
-    body: message.body,
-  ));
-}
-
-Future<void> _initializeMessaging() async {
-  if (await AwesomeNotifications().initialize(null, [
-    NotificationChannel(
-        channelKey: 'goambulance',
-        channelName: 'goambulance notifications',
-        channelDescription: 'Notification channel for goambulance',
-        defaultColor: Colors.black,
-        ledColor: Colors.white)
-  ])) {
-    if (kDebugMode) print('awesome notification initialized');
-  }
-  final messaging = FirebaseMessaging.instance;
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-  await AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
-    if (!isAllowed) {
-      AwesomeNotifications().requestPermissionToSendNotifications();
-    }
-  });
-  if (kDebugMode) {
-    print('User granted permission: ${settings.authorizationStatus}');
   }
 }
---
+++ lib/src/general/notifications.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/app_init_constants.dart';
+
+Future<void> initializeNotification() async {
+  if (!AppInit.isWeb) {
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      if (kDebugMode) {
+        print("Handling a foreground message: ${message.notification?.title}");
+      }
+      await _initializeMessaging();
+      await _createNotifications(
+          message.notification!, Random().nextInt(10000));
+    });
+    await FirebaseMessaging.instance.getInitialMessage();
+  }
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  if (kDebugMode) {
+    print("Handling a background message: ${message.notification?.title}");
+  }
+}
+
+Future<void> _createNotifications(RemoteNotification message, int id) async {
+  await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+    id: id,
+    channelKey: 'goambulance',
+    title: message.title,
+    body: message.body,
+  ));
+}
+
+Future<void> _initializeMessaging() async {
+  if (await AwesomeNotifications().initialize(null, [
+    NotificationChannel(
+        channelKey: 'goambulance',
+        channelName: 'goambulance notifications',
+        channelDescription: 'Notification channel for goambulance',
+        defaultColor: Colors.black,
+        ledColor: Colors.white)
+  ])) {
+    if (kDebugMode) print('awesome notification initialized');
+  }
+  final messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  await AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+    if (!isAllowed) {
+      AwesomeNotifications().requestPermissionToSendNotifications();
+    }
+  });
+  if (kDebugMode) {
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
+}

f84aef1200528d4de50b53c98d63990c340e7e6a
mosherif21 <mo.sherif1992@gmail.com>
2022-12-28 19:28:01 +0200

    initial commit

--- lib/src/constants/assets_strings.dart
+++ lib/src/constants/assets_strings.dart
@@ -2,29 +2,20 @@
 
 //--Images Assets
 const String kLogoImage = "assets/images/logo.png";
-const String kLogoImageWithSlogan = "assets/images/logo_with_name.png";
+const String kLogoImageWithSlogan =
+    "assets/images/logo_with_slogan_transparent.png";
 const String kSAFlagImage = "assets/images/saudi_arabia_flag.gif";
 const String kUkFlagImage = "assets/images/uk_flag.gif";
 const String kNotAvailableErrorAnim = "assets/images/error_beach.gif";
 
 //--Lottie assets
-const String kOnBoardingAnim1 = "assets/lottie_animations/iot.json";
+const String kOnBoardingAnim1 = "assets/lottie_animations/ambulance.json";
 const String kOnBoardingAnim2 =
-    "assets/lottie_animations/cloud_computing_security.json";
-const String kOnBoardingAnim3 =
-    "assets/lottie_animations/website_security.json";
-const String kOnBoardingAnim4 =
-    "assets/lottie_animations/customer_service.json";
-const String kOnBoardingAnim5 = "assets/lottie_animations/fridge_item.json";
+    "assets/lottie_animations/customer_service_test_blue.json";
+const String kOnBoardingAnim3 = "assets/lottie_animations/location_on_map.json";
 const String kEmailVerificationAnim =
     "assets/lottie_animations/email_verification.json";
 const String kPhoneVerificationAnim = "assets/lottie_animations/otp_code2.json";
 const String kPhoneOTPAnim = "assets/lottie_animations/otp_code3.json";
 const String kEmailOTPAnim = "assets/lottie_animations/mail_verification.json";
 const String kNoInternetAnim = "assets/lottie_animations/no_internet.json";
-
-const String kWebImage = "assets/images/web.png";
-const String kRobotImage = "assets/images/robot.png";
-const String kWarningImage = "assets/images/warning.png";
-
-const String kRefrigeratorTray = "assets/images/minibar.png";

7b87fbd033948bf1c0a77cce7778cf68ce41abf4
mosherif21 <mo.sherif1992@gmail.com>
2022-12-28 20:26:33 +0200

    initial commit

--- lib/src/features/home_page/screens/home_page_screen.dart
+++ lib/src/features/home_page/screens/home_page_screen.dart
@@ -5,6 +5,6 @@ class HomePageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: Text('Home Page'));
   }
 }

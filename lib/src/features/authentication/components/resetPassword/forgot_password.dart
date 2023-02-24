930e32a79a139c0e2355f71b3c001531a24f4afb
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-02-24 22:53:17 +0200

    initial commit

--- lib/src/features/authentication/components/resetPassword/forgot_password.dart
+++ lib/src/features/authentication/components/resetPassword/forgot_password.dart
@@ -19,7 +19,7 @@ class ForgetPasswordLayout extends StatelessWidget {
         children: [
           Text(
             'chooseForgetPasswordMethod'.tr,
-            style: Theme.of(context).textTheme.headline5,
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
           SizedBox(height: screenHeight * 0.02),
           FramedIconButton(

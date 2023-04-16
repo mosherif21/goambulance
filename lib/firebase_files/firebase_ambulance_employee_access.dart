cf7c76daa2e25fad0fe5ede3c24ccdb0df313c5a
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-04-16 04:19:45 +0200

    init

--- lib/firebase_files/firebase_ambulance_employee_access.dart
+++ lib/firebase_files/firebase_ambulance_employee_access.dart
@@ -1,9 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+
+import '../authentication/authentication_repository.dart';
 
 class FirebaseAmbulanceEmployeeDataAccess extends GetxController {
   static FirebaseAmbulanceEmployeeDataAccess get instance => Get.find();
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
+  late final String? userId;
+  late final FirebaseFirestore fireStore;
+  late final FirebaseDatabase fireDatabase;
+  late final FirebaseStorage fireStorage;
+  late final DocumentReference firestoreUserRef;
+  late final Reference userStorageReference;
+  @override
+  void onInit() {
+    super.onInit();
+    userId = AuthenticationRepository.instance.fireUser.value?.uid;
+    fireStore = FirebaseFirestore.instance;
+    fireDatabase = FirebaseDatabase.instance;
+    fireStorage = FirebaseStorage.instance;
+
+    firestoreUserRef = fireStore.collection('users').doc(userId!);
+    userStorageReference = fireStorage.ref().child('users').child(userId!);
+  }
 }

ba9618395753adb88f13d8c8bac4b0d103174e5f
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-05-28 20:22:14 +0300

    init

---
+++ lib/src/features/requests/controllers/request_history_details_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class RequestsHistoryDetailsController extends GetxController {
+  static RequestsHistoryDetailsController get instance => Get.find();
+
+  @override
+  void onReady() async {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}

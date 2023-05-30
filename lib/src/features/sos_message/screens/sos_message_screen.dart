eb38e85d692069c6057d76af7f967afb63a44038
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-05-30 16:27:54 +0300

    init

--- lib/src/features/sos_message/screens/sos_message_screen.dart
+++ lib/src/features/sos_message/screens/sos_message_screen.dart
@@ -37,7 +37,7 @@ class SosMessageScreen extends StatelessWidget {
       backgroundColor: Colors.grey.shade100,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
           child: StretchingOverscrollIndicator(
             axisDirection: AxisDirection.down,
             child: SingleChildScrollView(

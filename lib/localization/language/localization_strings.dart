9cd28f4eda231a3ac30f40f82e1bf10559f79e00
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-06-25 17:01:13 +0300

    init

--- lib/localization/language/localization_strings.dart
+++ lib/localization/language/localization_strings.dart
@@ -419,11 +419,11 @@ class Languages extends Translations {
           'shakeToSos': 'تمكين اهتزاز الجهاز لإرسال طلب استغاثة',
           'failedToChangeSetting': 'فشل تغيير الإعداد ، يرجى المحاولة مرة أخرى',
           'sosRequestHospitalMaybeCanceled':
-              'ربما تم إلغاء طلب الاستغاثة لهذه المستشفى ونحن نبحث عن مستشفى آخرى',
+              'ربما تم إلغاء طلب الاستغاثة لهذه المستشفى',
         },
         'en_US': {
           'sosRequestHospitalMaybeCanceled':
-              'The SOS request to this hospital may have been canceled and we are searching for another hospital',
+              'The SOS request to this hospital may have been canceled',
           'failedToChangeSetting': 'Failed to change setting, please try again',
           'sendSosSms': 'Send sms message to emergency contacts',
           'shakeToSos': 'Enable shake device to make an SOS request',

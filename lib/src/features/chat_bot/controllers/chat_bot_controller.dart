0cba89795992c4eeee9910607252f4deddc8a9d8
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-06-26 16:28:03 +0300

    init

---
+++ lib/src/features/chat_bot/controllers/chat_bot_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+class ChatBotController extends GetxController {
+  static ChatBotController get instance => Get.find();
+
+  @override
+  void onReady() async {
+    super.onReady();
+  }
+
+  @override
+  void onClose() async {
+    super.onClose();
+  }
+}

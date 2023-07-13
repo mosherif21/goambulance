30f74030f58a0e53c9cf8804c39789faeb2113d2
mosherif21 <95943033+mosherif21@users.noreply.github.com>
2023-07-14 01:50:59 +0300

    init

--- lib/src/features/home_screen/components/home_navigation_bar.dart
+++ lib/src/features/home_screen/components/home_navigation_bar.dart
@@ -1,11 +1,6 @@
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
-import 'package:floating_draggable_widget/floating_draggable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:goambulance/src/constants/assets_strings.dart';
-import 'package:goambulance/src/general/app_init.dart';
-import 'package:goambulance/src/general/general_functions.dart';
-import 'package:lottie/lottie.dart';
 
 import '../../account/screens/account_screen.dart';
 import '../../home_dashboard/screens/home_dashboard.dart';
@@ -18,40 +13,43 @@ class HomeNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final homeScreenController = HomeScreenController.instance;
-    final screenHeight = getScreenHeight(context);
-    final screenWidth = getScreenWidth(context);
-    return FloatingDraggableWidget(
-      floatingWidget: Transform.scale(
-        scale: 1.5,
-        child: Lottie.asset(
-          kChatBotAnim,
-          fit: BoxFit.fill,
-          repeat: true,
-        ),
-      ),
-      // OpenContainer(
-      //   useRootNavigator: true,
-      //   closedElevation: 0,
-      //   openElevation: 0,
-      //   closedColor: Colors.transparent,
-      //   openColor: Colors.transparent,
-      //   closedBuilder: (context, action) => Transform.scale(
-      //     scale: 1.6,
-      //     child: Lottie.asset(
-      //       kChatBotAnim,
-      //       fit: BoxFit.fill,
-      //       repeat: true,
-      //     ),
-      //   ),
-      //   openBuilder: (context, action) => const ChatBotScreen(),
-      // ),
-      autoAlign: true,
-      floatingWidgetHeight: 130,
-      floatingWidgetWidth: AppInit.isWeb ? 130 : screenWidth * 0.3,
-      dx: AppInit.isWeb ? 0.0 : screenWidth * 0.72,
-      dy: screenHeight * 0.735,
-      mainScreenWidget: Scaffold(
-        body: Stack(children: [
+    // final screenHeight = getScreenHeight(context);
+    // final screenWidth = getScreenWidth(context);
+    return
+        // FloatingDraggableWidget(
+        // floatingWidget: Transform.scale(
+        //   scale: 1.5,
+        //   child: Lottie.asset(
+        //     kChatBotAnim,
+        //     fit: BoxFit.fill,
+        //     repeat: true,
+        //   ),
+        // ),
+        // OpenContainer(
+        //   useRootNavigator: true,
+        //   closedElevation: 0,
+        //   openElevation: 0,
+        //   closedColor: Colors.transparent,
+        //   openColor: Colors.transparent,
+        //   closedBuilder: (context, action) => Transform.scale(
+        //     scale: 1.6,
+        //     child: Lottie.asset(
+        //       kChatBotAnim,
+        //       fit: BoxFit.fill,
+        //       repeat: true,
+        //     ),
+        //   ),
+        //   openBuilder: (context, action) => const ChatBotScreen(),
+        // ),
+        // autoAlign: true,
+        // floatingWidgetHeight: 130,
+        // floatingWidgetWidth: AppInit.isWeb ? 130 : screenWidth * 0.3,
+        // dx: AppInit.isWeb ? 0.0 : screenWidth * 0.72,
+        // dy: screenHeight * 0.735,
+        // mainScreenWidget:
+        Scaffold(
+      body: Stack(
+        children: [
           Obx(
             () => homeScreenController.navBarIndex.value == 0
                 ? const HomeDashBoard()
@@ -92,9 +90,10 @@ class HomeNavigationBar extends StatelessWidget {
               ),
             ),
           ),
-        ]),
-        backgroundColor: Colors.grey.shade100,
+        ],
       ),
+      backgroundColor: Colors.grey.shade100,
+      // ),
     );
   }
 }
